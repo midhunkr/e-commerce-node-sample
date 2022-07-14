e654258f524f0e26de6a56d8cf9bb2dcd563bfca
Midhun KR <midhun.kr@inapp.com>
2022-07-14 20:05:10 +0530

    fetching from db product data completed

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,25 +4,13 @@ const mongoose=require("mongoose");
 const Product=require("../models/product");
 
 router.get("/",(req,res)=>{
-    res.status(200).json([ {
-        "title": "Asparagus",
-        "type": "vegetable",
-        "description": "Asparagus with ham on the wooden table",
-        "filename": "2.jpg",
-        "height": 450,
-        "width": 299,
-        "price": 18.95,
-        "rating": 3
-      }, {
-        "title": "Green smoothie",
-        "type": "dairy",
-        "description": "Glass of green smoothie with quail egg's yolk, served with cocktail tube, green apple and baby spinach leaves over tin surface.",
-        "filename": "3.jpg",
-        "height": 600,
-        "width": 399,
-        "price": 17.68,
-        "rating": 4
-      },])
+    Product.find().exec().then((result)=>{
+        res.status(201).json(result);
+    }).catch((err)=>{
+        res.status(500).json({
+            "error":"Unable to get the products"
+        });
+    });
 });
 
 router.post("/",(req,res)=>{
@@ -44,9 +32,16 @@ router.post("/",(req,res)=>{
    
 })
 router.get("/:productId",(req,res,next)=>{
-    res.status(200).json({
-        data:req.params.productId
+    Product.findById(req.params.productId).exec().then(result=>{
+        res.status(200).json({
+            data:result
+        })
+    }).catch(err=>{
+        res.status(400).json({
+            "message":"Unable to fetch product"
+        })
     })
+    
 })
 router.patch("/:productId",(req,res,next)=>{
     res.status(200).json({
