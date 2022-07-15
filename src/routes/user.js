8f30d879221472afe94109d0ba7884e588a8b245
Midhun KR <midhun.kr@inapp.com>
2022-07-15 19:11:40 +0530

    duplicate check for email

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,34 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const bcrypt=require("bcrypt");
-const User=require("../models/user")
+const bcrypt = require("bcrypt");
+const User = require("../models/user")
 
-router.post('/signup',(req,res,next)=>{
-    //10 rounds of salt is safe
-    if(!req.body.email||!req.body.password){
+router.post('/signup', (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
         return res.status(400).json("Bad request")
     }
-    bcrypt.hash(req.body.password,10,(err,hash)=>{
-        if(err){
+    User.find({ email: req.body.email }).exec().then(result => {
+        if (result.length) {
+            return res.status(409).json({
+                "message": "Duplicate data exits"
+            })
+        } else {
+            //10 rounds of salt is safe
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
 
-        }else{
-            const newUser=new User({
-                _id: new mongoose.Types.ObjectId(),
-                email:req.body.email,
-                password:hash
-            }).save().then(result=>{
-                res.status(201).json({
-                    "message":"User is created"
-                })
-            }).catch(error=>{
-                res.status(500).json({
-                    "error":"Internal error"
-                })
+                } else {
+                    const newUser = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    }).save().then(result => {
+                        res.status(201).json({
+                            "message": "User is created"
+                        })
+                    }).catch(error => {
+                        res.status(500).json({
+                            "error": "Internal error"
+                        })
+                    })
+                }
             })
         }
+    }).catch(error => {
+
     })
-   
-}); 
+
+
+
+
+});
 
 module.exports = router;
