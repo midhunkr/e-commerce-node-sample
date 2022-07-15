685b03554a84e377506e23e72d3983fb7e86797e
Midhun KR <midhun.kr@inapp.com>
2022-07-15 15:55:11 +0530

    delete product completed

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,59 +1,80 @@
-const express=require("express");
-const router=express.Router();
-const mongoose=require("mongoose");
-const Product=require("../models/product");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const Product = require("../models/product");
 
-router.get("/",(req,res)=>{
-    Product.find().exec().then((result)=>{
+router.get("/", (req, res) => {
+    Product.find().exec().then((result) => {
         res.status(201).json(result);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.status(500).json({
-            "error":"Unable to get the products"
+            "error": "Unable to get the products"
         });
     });
 });
 
-router.post("/",(req,res)=>{
-    const newProduct=new Product({
-        _id:new mongoose.Types.ObjectId(),
-        name:req.body.name,
-        price:req.body.price
-    });
-    newProduct.save().then((result)=>{
-        res.status(201).json({
-            "message":"Product created successfully"
+router.post("/", (req, res) => {
+    if (req.body.name && req.body.price) {
+        const newProduct = new Product({
+            _id: new mongoose.Types.ObjectId(),
+            name: req.body.name,
+            price: req.body.price
+        });
+        newProduct.save().then((result) => {
+            res.status(201).json({
+                "message": "Product created successfully"
+            })
+        }).catch((err) => {
+            res.status(400).json({
+                "message": "Unable to create product"
+            })
         })
-    }).catch((err)=>{
+    } else {
         res.status(400).json({
-            "message":"Unable to create product"
+            message: "Bad request"
         })
-    })
-    console.log("res is",req.body);
-   
+    }
+
+    console.log("res is", req.body);
+
 })
-router.get("/:productId",(req,res,next)=>{
-    Product.findById(req.params.productId).exec().then(result=>{
+router.get("/:productId", (req, res, next) => {
+    Product.findById(req.params.productId).exec().then(result => {
         res.status(200).json({
-            data:result
+            data: result
         })
-    }).catch(err=>{
+    }).catch(err => {
         res.status(400).json({
-            "message":"Unable to fetch product"
+            "message": "Unable to fetch product"
         })
     })
-    
+
 })
-router.patch("/:productId",(req,res,next)=>{
+router.patch("/:productId", (req, res, next) => {
     res.status(200).json({
-        data:req.params.productId,
-        method:"patch"
+        data: req.params.productId,
+        method: "patch"
     })
 })
-router.delete("/:productId",(req,res,next)=>{
-    res.status(200).json({
-        data:req.params.productId,
-        method:"deleted"
-    })
+router.delete("/:productId", (req, res, next) => {
+    const id = req.params.productId;
+    if (id) {
+        Product.deleteOne({ _id: id }).exec().then(result => {
+            res.status(200).json({
+                "message": "product deleted"
+            })
+        }).catch(error => {
+            res.status(500).json({
+                "message": "Unable to delete product"
+            })
+        })
+    }
+    else {
+        res.status(400).json({
+          "message":"Bad request"
+        })
+    }
+
 })
 
-module.exports=router;
+module.exports = router;
